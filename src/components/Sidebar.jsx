88586d93ed3a7da32a57a7ed3a45a097bd49a993
callmejojoe <legendaryjoezy@gmail.com>
2026-05-19 11:33:00 +0100

    added a ternary to the className in the sidebar component to make it easy to have an active state

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export default function Sidebar() {
+export default function Sidebar(page) {
   return (
     <div className="sidebar">
-      <button className="sidebar-btn">Front</button>
-      <button className="sidebar-btn">Queue</button>
-      <button className="sidebar-btn">History</button>
-      <button className="sidebar-btn">Settings</button>
+      <button className={page == 'home' ? "active" : "sidebar-btn"}>Home</button>
+      <button className={page == 'queue' ? "active" : "sidebar-btn"}>Queue</button>
+      <button className={page == 'history' ? "active" : "sidebar-btn"}>History</button>
+      <button className={page == 'settings' ? "active" : "sidebar-btn"}>Settings</button>
     </div>
   );
 }
